e6d52ca712e6648c19c7ad1a31dbb911f45ee91f
alexpalm8 <alex.palm8@gmail.com>
2018-04-14 16:45:42 -0500

    Add files via upload

--- server.c
+++ server.c
@@ -21,31 +21,21 @@ int main(int argc, char *argv[])
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
-     if (argc < 2) {
+     if (argc < 2) 
+	 {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     //checking if udp or tcp using strcmp
-	char udp1[3] = "udp";
+	//checking if udp or tcp using strcmp
 	char tcp1[3] = "tcp";
+	char udp1[3] = "udp";
 	
-	//start tcp//
-	if (strcmp(argv[2], tcp1) == 0)
+	//----------------------------------start TCP------------------------//
+	if (strcmp(argv[2], tcp1) == 0) //checks to see if tcp was in commandline 
 	{ 
-    portno = atoi(argv[2]);	
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
         error("ERROR opening socket");
-	} //end tcp//
-	//start udp//
-	else if (strcmp(argv[2], udp1) == 0)
-	{ 
-    portno = atoi(argv[2]);	
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-	} //end udp//
-	
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
@@ -67,7 +57,39 @@ int main(int argc, char *argv[])
      printf("Here is the message: %s\n",buffer);
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
+	}
+	//----------------------------------end TCP---------------------------//
+	
+	//---------------------------------start UDP--------------------------//
+	else if (strcmp(argv[2], udp1) == 0) //checks to see if udp was in commandline
+	{
+	 sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+	}
      close(newsockfd);
      close(sockfd);
      return 0; 
+	 
 }

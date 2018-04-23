54d04982e3c40c601e51e1e12af42df87e2fc373
alexpalm8 <alex.palm8@gmail.com>
2018-04-22 22:25:54 -0500

    Add files via upload

--- client.c
+++ client.c
@@ -6,52 +6,272 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <arpa/inet.h>
+
+char gChecksum;
 
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
 }
+void askForFileUDP(int sockfd, unsigned int length, struct sockaddr_in server, struct sockaddr_in from)
+{
+	char buffer[20];
+	bzero(buffer, 20);
+	printf("What file do you want?\n");
+	int lengthOfFileName = read(0, buffer, 20); //read from user
+	sendto(sockfd, buffer, lengthOfFileName - 1, 0,(const struct sockaddr *)&server,length); //send user input to server
+	bzero(buffer, 20);
+	
+	recvfrom(sockfd, buffer, 20, 0,(struct sockaddr *)&from, &length); //read in whether the file exists or not
+	write(1, buffer, 20); //write out server response to stdout
+	bzero(buffer, 20);
+	recvfrom(sockfd, buffer, 1, 0,(struct sockaddr *)&from, &length); //read in the checksum
+	gChecksum = buffer[0];
+	printf("\nHere is the server side checksum for your file: %u\n", buffer[0]);
+//	write(1, buffer, 6); //write checksum to user
+	bzero(buffer, 20);
+}
+void getFileUDP(int socket, unsigned int length, struct sockaddr_in server, struct sockaddr_in from)
+{
+	int numBytes = 10000, i;
+	char fileData[10000];
+	bzero(fileData, 10000);
+	char fileName[20];
+	bzero(fileName, 20);
+	FILE* file;
+	
+	//read the name of the file requested from socket
+	//open the file in client's folder
+	recvfrom(socket, fileName, 20, 0, (struct sockaddr *)&from, &length);
+	file = fopen(fileName, "w+");
+	fseek(file, 0, SEEK_SET); //set to start of file
+	
+	//start reading in data
+	while(numBytes == 10000)
+	{
+		numBytes = recvfrom(socket, fileData, 10000, 0, (struct sockaddr *)&from, &length);
+//		printf("Got %d bytes to write into my file\n", numBytes);
+		//write received data into new file
+		fwrite(fileData, 1, numBytes, file);
+	}
+	
+	//calculate checksum
+	fseek(file, 0, SEEK_END);
+	int sizeOfFile = ftell(file);
+//	printf("The size of the file is %d\n", sizeOfFile);
+	
+	fseek(file, 0, SEEK_SET);
+	unsigned char checksum = 0;
+	char * charFromFile = malloc(1);
+	
+	//walk through the file
+	//read in one char at a time
+	//subtract that char from the checksum char
+	//divide in order to notice if packets come in in the wrong order
+	for(i = 0; i < sizeOfFile; i++)
+	{
+		fread(charFromFile, 1, 1, file);
+		fseek(file, i, SEEK_SET);
+		checksum -= *charFromFile;
+		checksum = checksum/ 2;
+	}
+	//free memory from malloc call
+	free(charFromFile);
+	printf("Here is the client version of the checksum: %u\n", checksum);
+//	printf("Here is the global checksum: %u\n", gChecksum);
+	if(checksum == gChecksum)
+		printf("Checksums match, file successfully transferred\n");
+	else
+		printf("Checksums do not match, something went wrong\n");
+}
+
+void askForFile(int sockfd)
+{
+	char buffer[20];
+	bzero(buffer, 20);
+//	printf("What file do you want from the server?\n");
+	write(1, "What file you want? ", 20);
+	int lengthOfFileName = read(0, buffer, 20); //read from user
+	write(sockfd, buffer, lengthOfFileName - 1); //send user input to server
+	bzero(buffer, 20);
+	
+	read(sockfd, buffer, 20); //read in whether the file exists or not
+	write(1, buffer, 20); //write out server response to stdout
+	bzero(buffer, 20);
+	read(sockfd, buffer, 1); //read in the checksum
+	gChecksum = buffer[0];
+	printf("\nHere is the server side checksum for your file: %u\n", buffer[0]);
+//	write(1, buffer, 6); //write checksum to user
+	bzero(buffer, 20);
+}
+void getFile(int socket)
+{
+	int numBytes = 10000, i;
+	char fileData[10000];
+	bzero(fileData, 10000);
+	char fileName[20];
+	bzero(fileName, 20);
+	FILE* file;
+	
+	//read the name of the file requested from socket
+	//open the file in client's folder
+	read(socket, fileName, 20);
+	file = fopen(fileName, "w+");
+	fseek(file, 0, SEEK_SET); //set to start of file
+	
+	//start reading in data
+	while(numBytes == 10000)
+	{
+		numBytes = read(socket, fileData, 10000);
+//		printf("Got %d bytes to write into my file\n", numBytes);
+		//write received data into new file
+		fwrite(fileData, 1, numBytes, file);
+	}
+	
+	//calculate checksum
+	fseek(file, 0, SEEK_END);
+	int sizeOfFile = ftell(file);
+//	printf("The size of the file is %d\n", sizeOfFile);
+	
+	fseek(file, 0, SEEK_SET);
+	unsigned char checksum = 0;
+	char * charFromFile = malloc(1);
+	
+	//walk through the file
+	//read in one char at a time
+	//subtract that char from the checksum char
+	//divide in order to notice if packets come in in the wrong order
+	for(i = 0; i < sizeOfFile; i++)
+	{
+		fread(charFromFile, 1, 1, file);
+		fseek(file, i, SEEK_SET);
+		checksum -= *charFromFile;
+		checksum = checksum/ 2;
+	}
+	//free memory from malloc call
+	free(charFromFile);
+	printf("Here is the client version of the checksum: %u\n", checksum);
+//	printf("Here is the global checksum: %u\n", gChecksum);
+	if(checksum == gChecksum)
+		printf("Checksums match, file successfully transferred\n");
+	else
+		printf("Checksums do not match, something went wrong\n");
+}
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+	//checking if udp or tcp using strcmp
+	char tcp1[3] = "tcp";
+	char udp1[3] = "udp";
+	
+	//----------------------------------start TCP------------------------//
+	if (strcmp(argv[3], tcp1) == 0) //checks to see if tcp was in commandline 
+	{ 
+		int sockfd, portno, n;
+		struct sockaddr_in serv_addr;
+		struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
+		char buffer[256];
+		if (argc < 3) 
+		{
+			fprintf(stderr,"usage %s hostname port\n", argv[0]);
+			exit(0);
+		}	
+		
+		portno = atoi(argv[2]);	
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0) 
+		{
+			error("ERROR opening socket");
+		}
+		server = gethostbyname(argv[1]);
+		if (server == NULL) 
+		{
+			fprintf(stderr,"ERROR, no such host\n");
+			exit(0);
+		}
+		
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+		serv_addr.sin_port = htons(portno);
+		
+		if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+		{
+			error("ERROR connecting");
+		}
+		
+/*		printf("Please enter the message: ");
+		bzero(buffer,256);
+		fgets(buffer,255,stdin);
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0)
+		{			
+			error("ERROR writing to socket");
+		}
+		
+		bzero(buffer,256);
+		n = read(sockfd,buffer,255);
+		
+		if (n < 0) 
+		{
+			error("ERROR reading from socket");
+		}'*/
+
+		askForFile(sockfd);
+		getFile(sockfd);
+		close(sockfd);
+	} 
+	//----------------------------------end TCP---------------------------//
+	
+	//---------------------------------start UDP--------------------------//
+	else if (strcmp(argv[3], udp1) == 0) //checks to see if udp was in commandline
+	{ 
+		int sock, n;
+		unsigned int length;
+		struct sockaddr_in server, from;
+		struct hostent *hp;
+		char buffer[256];
+   
+		if (argc < 3) 
+		{ 
+			printf("Usage: server port\n");
+            exit(1);
+		}
+		
+		sock= socket(AF_INET, SOCK_DGRAM, 0);
+		if (sock < 0) 
+		{
+			error("socket");
+		}
+
+		server.sin_family = AF_INET;
+		hp = gethostbyname(argv[1]);
+		if (hp==0) error("Unknown host");
+
+		bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+		server.sin_port = htons(atoi(argv[2]));
+		length=sizeof(struct sockaddr_in);
+		
+/*		printf("Please enter the message: ");
+		bzero(buffer,256);
+		fgets(buffer,255,stdin);
+		n=sendto(sock,buffer, strlen(buffer),0,(const struct sockaddr *)&server,length);
+		
+		if (n < 0) error("Sendto");
+		n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
+		
+		if (n < 0) error("recvfrom");
+		write(1,"Got an ack: ",12);
+		write(1,buffer,n); */
+		
+		askForFileUDP(sock, length, server, from);
+		getFileUDP(sock, length, server, from);
+		close(sock);
+	} 
+	//----------------------------------end UDP-------------------------------//
+
     return 0;
 }
--- server.c
+++ server.c
@@ -14,42 +14,308 @@ void error(const char *msg)
     exit(1);
 }
 
+void createChecksumUDP(FILE* file, int size, int socket, struct sockaddr_in from, socklen_t fromlen)
+{
+	fseek(file, 0, SEEK_SET);
+	unsigned char checksum = 0;
+	char * charFromFile = malloc(1);
+	int i;
+	
+	//walk through the file
+	//read in one char at a time
+	//subtract that char from the checksum char
+	//divide in order to notice if packets come in in the wrong order
+	for(i = 0; i < size; i++)
+	{
+		fread(charFromFile, 1, 1, file);
+		fseek(file, i, SEEK_SET);
+		checksum -= *charFromFile;
+		checksum = checksum/ 2;
+	}
+	//free memory from malloc call
+	free(charFromFile);
+	//create a pointer so the checksum can be sent to client
+	char *pChecksum = &checksum;
+	sendto(socket, pChecksum, 1, 0, (struct sockaddr *)&from,&fromlen);
+	printf("Here is the checksum: %u\n", checksum);
+}
+
+void sendFileUDP(FILE* file, int size, int socket, struct sockaddr_in from, socklen_t fromlen)
+{
+	//IF THE SLEEP IS REMOVED IT WILL BREAK
+	sleep(1); //to make sure the fileName doesnt start reading data
+	int i;
+	char fileData[10000];
+	bzero(fileData, 10000);
+	int numBytesRead;
+	
+	//read into fileData buffer, then send to client via socket
+	fseek(file, 0, SEEK_SET);
+	for(i = 0; i * 10000 < size; i++)
+	{
+		numBytesRead = fread(fileData, 1, 10000, file);
+		sendto(socket, fileData, numBytesRead, 0, (struct sockaddr *)&from,&fromlen);
+		bzero(fileData, 10000);
+//		sleep(1);
+	}
+}
+
+void checkAndSendFileUDP(int newsockfd, struct sockaddr_in from, socklen_t fromlen)
+{
+	int i, n, lengthOfName;
+	FILE* data;	
+	char fileName[20];
+	lengthOfName = recvfrom(newsockfd, fileName, 20, 0, (struct sockaddr *)&from,&fromlen);
+		
+	fileName[lengthOfName] = '\0'; //convert to a string by adding a null terminator
+	printf("file name length: %d\n", lengthOfName);
+	printf("%s is the string you are looking for\n", fileName);
+	
+	if(access( fileName, F_OK ) != -1 ) 
+	{
+		//file exists
+		n = sendto(newsockfd, "File exists\n", 11, 0, (struct sockaddr *)&from,&fromlen);
+	} 
+	else 
+	{
+		//file does not exist
+		n = sendto(newsockfd, "File does not exist\n", 19, 0, (struct sockaddr *)&from,&fromlen);	
+		//this probably needs a pointer or something
+		//good luck future ian
+		//fileName is blank in the printf
+		exit(0);
+	}
+	
+	data = fopen(fileName, "r");
+		
+	
+	fseek(data, 0, SEEK_END);
+	int sizeOfFile = ftell(data); //get the length of the file
+	char buffer[sizeOfFile];
+	fseek(data, 0, SEEK_SET); //go back to the start of the file
+	
+	printf("%d is the size of the file\n", sizeOfFile);
+	
+	//generate a checksum - we want to get 6 chars from the file
+	//first char, then 1/5, 2,5, 3/5, 4/5, last
+	createChecksumUDP(data, sizeOfFile, newsockfd, from, fromlen);
+	sendto(newsockfd, fileName, lengthOfName, 0, (struct sockaddr *)&from,&fromlen); //send client requested file name so client can open the file
+	sendFileUDP(data, sizeOfFile, newsockfd, from, fromlen);
+	
+}
+
+void createChecksum(FILE* file, int size, int socket)
+{
+	fseek(file, 0, SEEK_SET);
+	unsigned char checksum = 0;
+	char * charFromFile = malloc(1);
+	int i;
+	
+	//walk through the file
+	//read in one char at a time
+	//subtract that char from the checksum char
+	//divide in order to notice if packets come in in the wrong order
+	for(i = 0; i < size; i++)
+	{
+		fread(charFromFile, 1, 1, file);
+		fseek(file, i, SEEK_SET);
+		checksum -= *charFromFile;
+		checksum = checksum/ 2;
+	}
+	//free memory from malloc call
+	free(charFromFile);
+	//create a pointer so the checksum can be sent to client
+	char *pChecksum = &checksum;
+	write(socket, pChecksum, 1);
+	printf("Here is the checksum: %u\n", checksum);
+}
+
+void sendFile(FILE* file, int size, int socket)
+{
+	//IF THE SLEEP IS REMOVED IT WILL BREAK
+	sleep(1); //to make sure the fileName doesnt start reading data
+	int i;
+	char fileData[10000];
+	bzero(fileData, 10000);
+	int numBytesRead;
+	
+	//read into fileData buffer, then send to client via socket
+	fseek(file, 0, SEEK_SET);
+	for(i = 0; i * 10000 < size; i++)
+	{
+		numBytesRead = fread(fileData, 1, 10000, file);
+		write(socket, fileData, numBytesRead);
+		bzero(fileData, 10000);
+//		sleep(1);
+	}
+}
+
+void checkAndSendFile(int newsockfd)
+{
+	int i, n, lengthOfName;
+	FILE* data;	
+	char fileName[20];
+	lengthOfName = read(newsockfd, fileName, 20);
+		
+	fileName[lengthOfName] = '\0'; //convert to a string by adding a null terminator
+	printf("file name length: %d\n", lengthOfName);
+	printf("%s is the string you are looking for\n", fileName);
+	
+	if( access( fileName, F_OK ) != -1 ) 
+	{
+		//file exists
+		n = write(newsockfd, "File exists\n", 11);
+	} 
+	else 
+	{
+		//file does not exist
+		n = write(newsockfd, "File does not exist\n", 19); 		
+		//this probably needs a pointer or something
+		//good luck future ian
+		//fileName is blank in the printf
+		exit(0);
+	}
+	
+	data = fopen(fileName, "r");
+		
+	
+	fseek(data, 0, SEEK_END);
+	int sizeOfFile = ftell(data); //get the length of the file
+	char buffer[sizeOfFile];
+	fseek(data, 0, SEEK_SET); //go back to the start of the file
+	
+	printf("%d is the size of the file\n", sizeOfFile);
+	
+	//generate a checksum - we want to get 6 chars from the file
+	//first char, then 1/5, 2,5, 3/5, 4/5, last
+	createChecksum(data, sizeOfFile, newsockfd);
+	write(newsockfd, fileName, lengthOfName); //send client requested file name so client can open the file
+	sendFile(data, sizeOfFile, newsockfd);
+	
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+	//checking if udp or tcp using strcmp
+	char tcp1[3] = "tcp";
+	char udp1[3] = "udp";
+	
+	//----------------------------------start TCP------------------------//
+	if (strcmp(argv[2], tcp1) == 0) //checks to see if tcp was in commandline 
+	{ 
+		int sockfd, newsockfd, portno;
+		socklen_t clilen;
+		char buffer[256];
+		struct sockaddr_in serv_addr, cli_addr;
+		int n;
+		
+		if (argc < 2) 
+		{
+			fprintf(stderr,"ERROR, no port provided\n");
+			exit(1);
+		}
+	
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		
+		if (sockfd < 0) 
+		{
+			error("ERROR opening socket");
+		}
+		
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+		portno = atoi(argv[1]);
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(portno);
+		
+		if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+		{
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
+		}
+		
+		listen(sockfd,5);
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		
+		if (newsockfd < 0) 
+		{
           error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+		}
+		
+/*		bzero(buffer,256);
+		n = read(newsockfd,buffer,255);
+		if (n < 0)
+		{
+			error("ERROR reading from socket");
+		}
+		
+		printf("Here is the message: %s\n",buffer);
+		n = write(newsockfd,"I got your message",18);
+		
+		if (n < 0)
+		{			
+			error("ERROR writing to socket");
+		}
+*/
+		checkAndSendFile(newsockfd);
+		
+		close(newsockfd);
+		close(sockfd);
+	}
+	//----------------------------------end TCP---------------------------//
+	
+	//---------------------------------start UDP--------------------------//
+	else if (strcmp(argv[2], udp1) == 0) //checks to see if udp was in commandline
+	{
+		int sock, length, n;
+		socklen_t fromlen;
+		struct sockaddr_in server;
+		struct sockaddr_in from;
+		char buf[1024];
+
+		if (argc < 2) 
+		{
+			fprintf(stderr, "ERROR, no port provided\n");
+			exit(0);
+		}
+   
+		sock=socket(AF_INET, SOCK_DGRAM, 0);
+		if (sock < 0) 
+		{
+			error("Opening socket");
+		}
+		
+		length = sizeof(server);
+		bzero(&server,length);
+		server.sin_family=AF_INET;
+		server.sin_addr.s_addr=INADDR_ANY;
+		server.sin_port=htons(atoi(argv[1]));
+		if (bind(sock,(struct sockaddr *)&server,length)<0) 
+		{
+			error("binding");
+		}
+		
+		fromlen = sizeof(struct sockaddr_in);
+/*		while (1) 
+		{
+			n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
+			if (n < 0)
+			{				
+				error("recvfrom");
+			}
+			
+			write(1,"Received a datagram: ",21);
+			write(1,buf,n);
+			n = sendto(sock,"Got your message\n",17, 0,(struct sockaddr *)&from,fromlen);
+			
+			if (n  < 0) 
+			{
+				error("sendto");
+			}
+		}*/
+		
+		checkAndSendFile(sock);
+	}
+	
+    return 0;  
 }
